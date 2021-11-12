f0169dadf34fb62a14ef1b2d7b93bd7f98b15a70
João Paulo <joaopauloribeiropontes@gmail.com>
2021-11-12 02:57:21 -0300

    build.sh run.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
 docker build -t shipsmart-crud-front-node .
-docker run -it -p 8089:8080 --rm --name shipsmart-crud-front shipsmart-crud-front-node
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -it -p 8089:8080 --rm --name shipsmart-crud-front shipsmart-crud-front-node
